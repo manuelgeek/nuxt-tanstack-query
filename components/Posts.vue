79aadc7c5e08708c057187aaae9d43037de5798e
Magak Emmanuel <emashmagak@gmail.com>
2024-08-13 17:05:35 +0300

    init, added vue-query

---
+++ components/Posts.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query';
+import type { Post } from '~/type';
+
+const getPosts = async (): Promise<Post[]> => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts")
+    return data.json()
+}
+
+const { data: posts, suspense } = useQuery({
+    queryKey: ['posts'],
+    queryFn: getPosts,
+})
+
+onServerPrefetch(async () => {
+    await suspense()
+})
+</script>
+<template>
+    <div>
+        <h1>All Posts</h1> <li v-for="post in posts" className="mt-3 border-b border-blue-500" :key=post.id>
+            {{post.title}}
+        </li>
+    </div>
+</template>
---
+++ type.ts
@@ -0,0 +1,10 @@
+export interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+export interface CustomError {
+  message: string
+}
